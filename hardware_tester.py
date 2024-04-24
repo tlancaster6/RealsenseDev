0d03f9154f1ca296105dee171572afb9d4151c5a
tlancaster6 <tuckerlancaster@gmail.com>
2024-04-24 13:36:09 -0400

    adding hardware testing code

---
+++ hardware_tester.py
@@ -0,0 +1,55 @@
+from recorder import Recorder, OUTPUT_DIR
+import pandas as pd
+from os.path import getsize
+import time
+
+
+class HardwareTester:
+
+    def __init__(self):
+        self.results = []
+
+    def run_test(self, framerate, framesize, test_length=10):
+        print(f'testing with framerate={framerate}, framesize={framesize}')
+        recorder = Recorder(framerate, framesize)
+        n_write_ops = 0
+        total_write_time = 0
+        end = time.time() + test_length
+        while time.time() <= end:
+            frame = recorder.aquire_frame()
+            write_start = time.time()
+            recorder.write_frame(frame)
+            total_write_time += time.time() - write_start
+            n_write_ops += 1
+        results = {
+            'target_framerate': framerate,
+            'resolution': framesize,
+            'test_len_secs': test_length,
+            'n_write_ops': n_write_ops,
+            'avg_write_time': total_write_time / n_write_ops,
+            'allowable_write_time': 1 / framerate,
+            'file_size_my': getsize(str(recorder.outfile)) * 10e-6
+        }
+        self.results.append(results)
+
+    def compile_results(self, save=True):
+        self.results = pd.DataFrame(self.results)
+        if save:
+            self.results.to_csv(str(OUTPUT_DIR / 'testing_results.csv'))
+
+
+if __name__ == "__main__":
+    tester = HardwareTester()
+    for framerate in [6, 15, 30]:
+        tester.run_test(framerate=framerate, framesize=(1280, 720), test_length=10)
+    for framerate in [6, 15, 30, 60, 90]:
+        for framesize in [(848, 480), (640, 480), (640, 360), (480, 720), (424, 240)]:
+            tester.run_test(framerate=framerate, framesize=framesize)
+    tester.compile_results()
+
+
+
+
+
+
+
--- recorder.py
+++ recorder.py
@@ -1,27 +1,75 @@
 import pyrealsense2 as rs
+import cv2
+import pathlib
+import numpy as np
+import time
+
+OUTPUT_DIR = pathlib.Path().home() / 'Videos'
+
+if not OUTPUT_DIR.exists():
+    OUTPUT_DIR.mkdir()
 
 
 class Recorder:
 
-    def __init__(self):
-        self.context = rs.context()
-        self.devices = self.context.devices
+    def __init__(self, framerate=30, framesize=(640, 480)):
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.output_dir = OUTPUT_DIR
+        self.rs_format = rs.format.y8
+        self.framerate = framerate
+        self.framesize = framesize
+        self.config, self.pipeline, self.profile, self.serial = self.enable_device()
+        self.outfile = OUTPUT_DIR / f"{self.serial}.avi"
+        self.writer = cv2.VideoWriter(str(self.outfile), self.fourcc, framerate, (framesize[0]*2, framesize[1]), 0)
+        self.framecount = 0
 
-    def configure_devices(self):
-        configs = []
-        for device in self.devices:
-            serial_number = device.get_info(rs.camera_info.serial_number)
-            config = rs.config()
-            config.enable_device(str(serial_number))
-            config.enable_stream(rs.stream.depth, 640, 480, rs.format.y8, 30)
-            config.enable_stream(rs.stream.color, 640, 480, rs.format.y8, 30)
-            configs.append(config)
-        return configs
-
-    def start(self, config):
+    def enable_device(self):
         pipeline = rs.pipeline()
-        pipeline.start(config)
-        return pipeline
+        config = rs.config()
+        config.enable_stream(rs.stream.infrared, 1, *self.framesize, self.rs_format, self.framerate)
+        config.enable_stream(rs.stream.infrared, 2, *self.framesize, self.rs_format, self.framerate)
+        profile = pipeline.start(config)
+
+        device = profile.get_device()
+        serial = device.get_info(rs.camera_info.serial_number)
+        depth_sensor = device.query_sensors()[0]
+        if depth_sensor.supports(rs.option.emitter_enabled):
+            depth_sensor.set_option(rs.option.emitter_enabled, 0)
+        return config, pipeline, profile, serial
+
+    def aquire_frame(self):
+        frames = self.pipeline.wait_for_frames()
+        ir1_frame = frames.get_infrared_frame(1)
+        ir2_frame = frames.get_infrared_frame(2)
+        image1 = np.asanyarray(ir1_frame.get_data())
+        image2 = np.asanyarray(ir2_frame.get_data())
+        return np.hstack([image1, image2])
+
+    def write_frame(self, frame):
+        self.writer.write(frame)
+        self.framecount += 1
+
+    def record(self, length=10):
+        end = time.time() + length
+        while time.time() <= end:
+            self.write_frame(self.aquire_frame())
+
+    def exit(self):
+        self.writer.release()
+        self.pipeline.stop()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.exit()
 
 
-recorder = Recorder()
+if __name__ == "__main__":
+    with Recorder() as recorder:
+        recorder.record()
+
+
+
+
+
